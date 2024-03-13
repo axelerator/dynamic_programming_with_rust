b13a6cee285f04735c51c9470483bd9084685980
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2024-03-13 14:51:49 -0400

    Add solution to exercise 2

---
+++ src/exercise2.rs
@@ -0,0 +1,25 @@
+use crate::read::get_i64;
+
+fn fibonacci(n: i64) -> i64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+pub fn main() {
+    println!("Enter -1 to exit\n");
+    loop {
+        // Prompt the user for n.
+        let n = get_i64("N: ");
+
+        // If n < 0, break out of the loop.
+        if n < 0 {
+            break;
+        }
+
+        // Calculate the Fibonacci number.
+        println!("fibonacci({}) = {}\n", n, fibonacci(n));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod exercise1;
+mod exercise2;
 
 mod read;
 
 fn main() {
-    exercise1::main();
+    // exercise1::main();
+    exercise2::main();
 }

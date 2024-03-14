8481814ac5964566aaa725f24f009ead05509050
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2024-03-14 18:09:33 -0400

    Add exhaustive solution for E5

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod exercise1;
 mod exercise2;
 mod exercise3;
 mod exercise4;
+mod exercise5;
 
 mod read;
 
@@ -9,5 +10,6 @@ fn main() {
     // exercise1::main();
     // exercise2::main();
     // exercise3::main();
-    exercise4::main();
+    // exercise4::main();
+    exercise5::main();
 }
